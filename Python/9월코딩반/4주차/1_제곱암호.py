32cfa673292d6004f9d14ff7c0840b5a00931b3f
Gayoung <38778937+Goinging98@users.noreply.github.com>
2023-09-12 21:03:41 +0900

    풀이 업뎃

--- Python/9월코딩반/4주차/1_제곱암호.py
+++ Python/9월코딩반/4주차/1_제곱암호.py
@@ -7,3 +7,35 @@
 # z에서 사전 기준 다음 문자로 바뀌면 a로 바뀐다. 
 # 복호화 끝난 뒤 원문은 N/2의 길이의 알파벳 소문자로만 이루어진 문자열 
 
+# 아스키코드 
+# 문자를 숫자로 ord / 숫자를 문자로 chr 
+
+# 내 풀이 
+# 이렇게 하면 문제점이 제곱한 값이 26을 몇 번이고 넘으면 안 된다는 것 
+# 그래서 에러 뜸 
+n = int(input())
+s = input()
+
+for i in range(int(n/2)) :
+	num = ord(s[2*i])
+	total = num + (int(s[2*i+1])**2)
+	if total > 122 :
+		total = total - 26
+	else : 
+		total = total 		
+	word = chr(total)
+	print(word, end = '')
+
+
+# 풀이
+n = int(input())
+s = input()
+res = ''
+
+for i in range(0, n, 2) :
+	c = s[i]
+	n = int(s[i+1])
+	# 26으로 나눠서 더해주기 
+	d = chr((ord(c) - ord('a') + n*n) % 26 + ord('a'))
+	res += d 
+print(res)
